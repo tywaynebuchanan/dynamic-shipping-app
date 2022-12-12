96b75f59cff5a6912ce5f3281b41260032759887
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2022-12-12 15:27:22 -0500

    made changes to mobile menu

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ const menu = document.getElementById("menu-icon");
 const navbar = document.querySelector(".navbar");
 const icon = document.querySelector(".ri-close-line")
 
-window.onload = function(){
+document.addEventListener('load',()=>{
     navbar.classList.remove("show");
-}
+})
+
 menu.addEventListener('click',()=>{
     navbar.classList.toggle("show");
 
