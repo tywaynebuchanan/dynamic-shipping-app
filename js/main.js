e629f163e991a964ffaaa10856a4cdac1e0f9087
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2022-12-12 14:11:29 -0500

    fixed mobile menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const menu = document.getElementById("menu-icon");
 const navbar = document.querySelector(".navbar");
+const icon = document.querySelector(".ri-close-line")
+
 
 menu.addEventListener('click',()=>{
     navbar.classList.toggle("show");
+    
 })
