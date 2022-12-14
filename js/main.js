333036331cd35eca14dfd6d205c249300ac7596f
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2022-12-14 12:25:03 -0500

    added the rates page and made it mobile friendly

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@ const menu = document.getElementById("menu-icon");
 const navbar = document.querySelector(".navbar");
 const icon = document.querySelector(".ri-close-line")
 
-document.addEventListener('load',()=>{
-    navbar.classList.remove("show");
-})
-
 
 menu.addEventListener('click',()=>{
     navbar.classList.toggle("show");
